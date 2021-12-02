6c1db41a155eb1c9fc449941eda7dc49e56a585c
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2021-12-02 22:59:14 +0100

    day 1 part 2
    https://adventofcode.com/2021/day/1#part2

--- 01-measurements.rkt
+++ 01-measurements.rkt
@@ -1,12 +1,19 @@
 #lang racket
 
-(define (increases m)
+(define (larger-increases m)
   (define increased
     (for/stream ([i m] [j (stream-rest m)] #:when (< i j)) 1))
   (stream-fold + 0 increased))
 
+(define (larger-sums m)
+  (define sums
+    (for/stream ([i m] [j (stream-rest m)] [k (stream-rest (stream-rest m))])
+      (+ i j k)))
+  (larger-increases sums))
+
 (define (count-larger-measurements-in file)
   (define m (stream-map string->number (sequence->stream (in-lines file))))
-  (increases m))
+  (printf  "~a measurements are larger than the previous measurement~n" (larger-increases m))
+  (printf  "~a sums are larger than the previous sum~n" (larger-sums m)))
 
 (call-with-input-file "01-input.txt" count-larger-measurements-in)
