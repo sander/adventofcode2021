5f2494e020def2aeba644fd033777a2dbd0c638b
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2021-12-04 11:40:11 +0100

    feature(02): part 2

--- 02-dive.rkt
+++ 02-dive.rkt
@@ -40,9 +40,35 @@
   (check-equal? (parse-command "forward 10") '(forward 10))
   (check-equal? (parse-command "forward") #f))
 
+(define (move-with-aim command position)
+  (define h (dict-ref position 'horizontal))
+  (define d (dict-ref position 'depth))
+  (define a (dict-ref position 'aim))
+  (define v (cadr command))
+  (case (car command)
+    [(forward) (dict-set* position 'horizontal (+ h v) 'depth (+ d (* a v)))]
+    [(down) (dict-set position 'aim (+ a v))]
+    [(up) (dict-set position 'aim (- a v))]))
+
+(module+ test
+  (define example-output-with-aim (position 15 60))
+  (define example-output-with-aim-horizontal 15)
+  (define example-output-with-aim-depth 60)
+
+  (define actual-output-moved-with-aim (foldl move-with-aim (position) example-input))
+
+  (define (check-equal-ref? d1 d2 k)
+    (check-equal? (dict-ref d1 k) (dict-ref d2 k)))
+
+  (check-equal-ref? actual-output-moved-with-aim example-output-with-aim 'horizontal)
+  (check-equal-ref? actual-output-moved-with-aim example-output-with-aim 'depth)
+  (check-equal? (position-multiplied example-output-with-aim) 900))
+
 (define (follow-planned-course-in file)
   (define m (map parse-command (sequence->list (in-lines file))))
   (define result (position-multiplied (foldl move (position) m)))
-  (printf "~a is the final horizontal position multipled by the final depth" result))
+  (define result-with-aim (position-multiplied (foldl move-with-aim (position) m)))
+  (printf "~a is the final horizontal position multipled by the final depth~n" result)
+  (printf "~a is the final horizontal position when moved with aim multipled by the final depth" result-with-aim))
 
 (call-with-input-file "02-input.txt" follow-planned-course-in)
